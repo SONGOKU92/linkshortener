97e3d2f6a217720c55f354f8e42c76cd6daf0751
Adrien <157978359+SONGOKU92@users.noreply.github.com>
2024-10-04 11:48:30 +0200

    Update script.py

--- script.py
+++ script.py
@@ -58,4 +58,4 @@ def redirect_to_long_url(short_id):
         return jsonify({'error': 'URL non trouvée'}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
